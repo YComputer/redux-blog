ce8214c6d2aee8bb66cc09ea77298dea3349ec7b
YComputer <thea@163.com>
2017-03-13 14:11:04 +0800

    just format

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,7 +1,7 @@
 import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
 import { routerReducer, routerMiddleware } from 'react-router-redux';
 import { hashHistory, browserHistory } from 'react-router';
-import thunkMiddleware from 'redux-thunk';
+import thunk from 'redux-thunk';
 import DevTools from './DevTools';
 import fetchMiddleware from './fetchMiddleware';
 
@@ -9,7 +9,7 @@ import rootReducer from './reducers';
 
 // 添加中间件
 const enhancedCreateStore = compose(
-    applyMiddleware(thunkMiddleware, fetchMiddleware, routerMiddleware(hashHistory)),
+    applyMiddleware(thunk, fetchMiddleware, routerMiddleware(hashHistory)),
     DevTools.instrument()
 )(createStore);
 
